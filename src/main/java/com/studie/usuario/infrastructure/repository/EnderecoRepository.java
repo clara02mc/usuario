90777dbc64727db0ee3dbffa979f1ab9675e861d
clara02mc <maria.cvm172002@gmail.com>
2025-12-29 00:00:44 -0300

    classes do security adicionadas

---
+++ src/main/java/com/studie/usuario/infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,12 @@
+package com.studie.usuario.infrastructure.repository;
+
+import com.javanauta2.springstudie.infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+    @Repository
+    public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+    }
+
+
---
+++ src/main/java/com/studie/usuario/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,8 @@
+package com.studie.usuario.infrastructure.repository;
+import com.javanauta2.springstudie.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+}
